362904497e2e537f502dbb8a724d1803858b2012
dheerajdevarapu-design <dheerajdevarapu@gmail.com>
2026-03-20 09:51:03 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ BookMyStay/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,12 @@
+public class UseCase1HotelBookingApp {
+
+    static String appName = "Book My Stay";
+    static String version = "Hotel Booking System v1.0";
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to " + appName);
+        System.out.println(version);
+
+    }
+}
