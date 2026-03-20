9b4df2d83fcd1fbd5a6988d5e91cb3a84eec7034
dheerajdevarapu-design <dheerajdevarapu@gmail.com>
2026-03-20 09:54:46 +0530

    Use Case 2: Basic Room Types & Static Availability

--- BookMyStay/src/UseCase1HotelBookingApp.java
+++ BookMyStay/src/UseCase1HotelBookingApp.java
@@ -1,12 +1,65 @@
-public class UseCase1HotelBookingApp {
+abstract class Room {
 
-    static String appName = "Book My Stay";
-    static String version = "Hotel Booking System v1.0";
+    String type;
+    int beds;
+    double price;
+
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println(type + " | Beds: " + beds + " | Price: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+
+    SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+}
+
+public class UseCase2RoomInitialization {
+
+    static String version = "Hotel Booking System v2.1";
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to " + appName);
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Book My Stay");
         System.out.println(version);
 
+        r1.display();
+        System.out.println("Available: " + singleAvailable);
+
+        r2.display();
+        System.out.println("Available: " + doubleAvailable);
+
+        r3.display();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
